f891bb71479d3ed6b26ca587acc9a1de4decec4b
superdef454 <superdef454@mail.ru>
2022-12-02 11:02:36 +0500

    modified:   matplotlib_2.py

--- matplotlib_2.py
+++ matplotlib_2.py
@@ -31,9 +31,7 @@ def x():
 def T(): # Время подвоза T определяется по нормальному закону
     Mx = 0 # математическое ожидание времени подвоза
     Sigmax = 0 # среднее квадратическое отклонение
-
     Zz = sum(R() for _ in range(1, 12)) - 6    # нормально распределенная случайная величина с параметрами
-    pass
 
 def Max_to_strah(): # Функция покупок товаров до страхового
     global Y_mas, T_mas
@@ -52,6 +50,8 @@ def Max_to_strah(): # Функция покупок товаров до стра
     
 Max_to_strah()
 # Отрисовка
+plt.title("Модель управления запасами")
+plt.xlabel("Время", loc="right")
 plt.scatter(T_peresech, [Y_strah_zapas for _ in range(len(T_peresech))]) # Можно сделать список пересечений list_per = [] \n list_per.append(t_mas[-1])
 plt.plot(T_mas, Y_mas)
 plt.plot([-1, T_mas[-1]],[Y_strah_zapas,Y_strah_zapas])
