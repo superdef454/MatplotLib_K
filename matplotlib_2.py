5e9e65fffcc050a5834d3f7b353521f56f2d4aa6
superdef454 <superdef454@mail.ru>
2022-12-02 10:52:57 +0500

    modified:   matplotlib_2.py

--- matplotlib_2.py
+++ matplotlib_2.py
@@ -1,8 +1,5 @@
 import random
 import math
-# import numpy
-# import matplotlib
-# import matplotlib.pyplot
 import matplotlib.pyplot as plt
 
 
@@ -31,15 +28,14 @@ def x():
     b = 3 # максимальное количество покупаемого товара
     return int(a+R()*(b-a))
 
-def T():
-    # Время подвоза T определяется по нормальному закону
+def T(): # Время подвоза T определяется по нормальному закону
     Mx = 0 # математическое ожидание времени подвоза
     Sigmax = 0 # среднее квадратическое отклонение
-    # нормально распределенная случайная величина с параметрами
-    Zz = sum(R() for _ in range(1, 12)) - 6
+
+    Zz = sum(R() for _ in range(1, 12)) - 6    # нормально распределенная случайная величина с параметрами
     pass
 
-def Max_to_strah():
+def Max_to_strah(): # Функция покупок товаров до страхового
     global Y_mas, T_mas
     Yy = Y_max
     t = 0
@@ -55,15 +51,9 @@ def Max_to_strah():
     T_peresech.append(T_mas[-1])
     
 Max_to_strah()
-
 # Отрисовка
-
 plt.scatter(T_peresech, [Y_strah_zapas for _ in range(len(T_peresech))]) # Можно сделать список пересечений list_per = [] \n list_per.append(t_mas[-1])
 plt.plot(T_mas, Y_mas)
 plt.plot([-1, T_mas[-1]],[Y_strah_zapas,Y_strah_zapas])
 plt.show()
-
-
-
-
 # Х - t || Y - Товар 
